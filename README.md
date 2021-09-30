5548a52d833fa137a6b9d46fc4966b8775999d18
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-09-30 16:19:36 +0530

    completed base

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>password generator </h1>
+
+<p> codes are written in pure python . for development and backend i choose django framework</p>
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def main(request):
+
+    return render(request,"main.html")
+
+def password_generator(request):
+
+    return render(request,"password_generator.html")
