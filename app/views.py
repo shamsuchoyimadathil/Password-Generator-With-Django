a4176ea06f24f56023f696fb22ee6e9c756f1c5f
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-10-06 12:08:30 +0530

    styling completed

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+import random
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -7,33 +8,41 @@ def main(request):
     return render(request,"main.html")
 
 def password_generator(request):
-    import random
-
+    context = {}
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    print("Welcome to the PyPassword Generator!")
-    nr_letters= int(input("How many letters would you like in your password?\n"))
-    nr_symbols = int(input(f"How many symbols would you like?\n"))
-    nr_numbers = int(input(f"How many numbers would you like?\n"))
+    if request.method == "POST":
+        letter = request.POST.get("pass_letter")
+        number = request.POST.get("pass_number")
+        symbol = request.POST.get("pass_symbol")
 
-    password = ""
-    for letter in range(1, nr_letters+1):
-        password = password+random.choice(letters)
+        password = ""
+        for a in range(1, int(letter)+1):
+            password = password+random.choice(letters)
+            print(password)
 
-    for symbol in range(1, nr_symbols+1):
-        password = password+random.choice(symbols)
-
-    for number in range(1, nr_numbers+1):
-        password = password+random.choice(numbers)
-
-    random_password = ""
-    for d in range(1, len(password)):
-        random_password_generator = random.choice(password)
-        random_password += random_password_generator
-    message = f" your password is: {random_password}"
-    print(message)
+        for b in range(1, int(number)+1):
+            password = password+random.choice(numbers)
+            print(password)
 
 
-    return render(request,"password_generator.html")
+        for c in range(1,int(symbol)+1):
+            password = password+random.choice(symbols)
+            print(password)
+
+
+        random_password = ""
+
+        for d in range(1, len(password)):
+            random_password_generator = random.choice(password)
+            random_password += random_password_generator 
+        message = f" your password is: {random_password}" 
+        context ["password"] = random_password
+        print(message)
+
+
+        return render(request,"password_generator.html",context)
+
+    return render(request,"password_generator.html",context)
