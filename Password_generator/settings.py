e1366bc51ec979a5bde3b84cd330da62ddf8516e
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-10-08 15:28:34 +0530

    collected static files

--- Password_generator/settings.py
+++ Password_generator/settings.py
@@ -111,14 +111,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-if DEBUG:
-   STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static/static_files'),
-]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static/static_files')
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_deploy')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'app/static'),
+)
 
 #Media
 MEDIA_URL = '/media/'
